9db3c0ac585d725d48ec9255002a36a842278313
Ahasanul Hoque <ahoque24@gmail.com>
2020-08-27 15:35:35 -0400

    Placed click functionality in an IIFE so currentTab does not have global scope

--- src/index.js
+++ src/index.js
@@ -10,23 +10,29 @@ loadHome(content);
 let navbar = document.querySelector('#navbar');
 let tabs = document.querySelectorAll('.tab');
 
-//Tab functionality
-let currentTab;
-navbar.onclick = (e) => {
-    let tab = e.target.getAttribute('id');
-    
-    
-    if(tab == 'home' && currentTab != 'home'){
-        content.removeChild(content.lastChild);
-        loadHome(content);
-        currentTab = 'home';
-    } else if (tab == 'menu' && currentTab != 'menu'){
-        content.removeChild(content.lastChild);
-        showMenu(content);
-        currentTab = 'menu';
-    } else if (tab == 'contact' && currentTab != 'contact'){
-        content.removeChild(content.lastChild);
-        showContactInfo(content);
-        currentTab = 'contact';
+
+const TabController = (() => {
+    //Tab functionality
+    //currentTab variable is used to determine whether the clicked tab is already
+    //on the screen. If it is, do nothing. Declared outside of click so it is not
+    //declared again each time the user clicks
+    let currentTab;
+    navbar.onclick = (e) => {
+        let tab = e.target.getAttribute('id');
+        
+        
+        if(tab == 'home' && currentTab != 'home'){
+            content.removeChild(content.lastChild);
+            loadHome(content);
+            currentTab = 'home';
+        } else if (tab == 'menu' && currentTab != 'menu'){
+            content.removeChild(content.lastChild);
+            showMenu(content);
+            currentTab = 'menu';
+        } else if (tab == 'contact' && currentTab != 'contact'){
+            content.removeChild(content.lastChild);
+            showContactInfo(content);
+            currentTab = 'contact';
+        }
     }
-}
+})();
